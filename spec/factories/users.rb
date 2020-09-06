8ff38e94c8707c03c809d14adfd4ce7d24dcaa18
wata6 <63787375+wata6@users.noreply.github.com>
2020-09-06 22:34:53 +0900

    Test/user requests #4 (#18)
    * feat:gem factory_bot_rails
    
    * test:spec/requests/users_spec.rb
    
    * fix:Gemfile
    
    * fix:users_spac.rb

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,4 +3,14 @@ FactoryBot.define do
     name { 'test' }
     sequence(:email) { 'Test1@example.com' }
   end
+
+  factory :invalid_user, class: User do
+    name { nil }
+    email { 'aaa' }
+  end
+
+  factory :user2, class: User do
+    name { 'testuser2' }
+    email { 'test2@example.com' }
+  end
 end
